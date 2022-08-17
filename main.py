e7b08364c63e7e9483faff4751ae61289de38368
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-08-17 13:03:43 +0530

    main_2 file added

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import re
 import requests
 import bs4
+
+
+result = requests.get("https://en.wikipedia.org/wiki/Grace_Hopper")
+
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+
+for every in soup.select('.toctext'):
+    print(every.text)
