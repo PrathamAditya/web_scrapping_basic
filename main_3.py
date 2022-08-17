f1fcc336928e1e408f6e7b40eea9c52ab47f89ee
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-08-17 15:03:59 +0530

    just another commit i m feeling sleepy

---
+++ main_3.py
@@ -0,0 +1,21 @@
+from cgi import print_directory
+from cgitb import reset
+import requests
+import bs4
+
+
+base_url = "http://books.toscrape.com/catalogue/page-"
+
+list_of_req_book = []
+
+for i in range(1, 51):
+    current_url = base_url + f"{i}" + ".html"
+    result = requests.get(f"{current_url}")
+    soup = bs4.BeautifulSoup(result.text, 'lxml')
+    list_book = soup.select(".product_pod")
+    for x in list_book:
+        if len(x.select(".star-rating.Two")) != 0:
+            xx = x.select('a')[1]['title']
+            list_of_req_book.append(xx)
+
+for each in list_of_req_book:
